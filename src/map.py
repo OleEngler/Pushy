7c2e0347661c9aa73ce38cdce3d130cf5f59134a
Ole Engler <ole.engler@WILTMANN.DE>
2025-06-13 13:02:48 +0200

    BaseGame

--- src/map.py
+++ src/map.py
@@ -1,11 +1,18 @@
-
+import json
+from pathlib import Path
 
 class Map():
-    def __init__(self):
+    def __init__(self,map_file_name=None):
+        self.name = None
         self.start_pos = None
         self.goal_pos = None
         self.layout = None
+        self.project_file = Path(__file__).resolve().parent.parent
+        self.map_file_name = map_file_name
 
+    def get_name(self):
+        return self.name
+    
     def get_start_pos(self):
         return self.start_pos
     
@@ -14,6 +21,9 @@ class Map():
     
     def get_layout(self):
         return self.layout
+    
+    def set_name(self,name):
+        self.name = name
     
     def set_start_pos(self,pos):
         self.start_pos = pos
@@ -25,24 +35,31 @@ class Map():
         self.layout = layout
 
     
-    def load_map(self,path=None):
-        if path == None:
+    def load_map(self):
+        if self.map_file_name == None:
             self.load_default_map()
         else:
-            pass #TODO: Mapsladen evtl: JSON 
+            with open(f"{self.project_file}\\maps\\{self.map_file_name}", "r", encoding="utf-8") as f:
+                daten = json.load(f)
+
+            self.set_name(daten["Name"])
+            self.set_start_pos(daten["Start_pos"])
+            self.set_goal_pos(daten["Goal_pos"])
+            self.set_layout(daten["Layout"])
 
 
     def load_default_map(self):
         self.set_start_pos([5,1])
         self.set_goal_pos([1,8])
         self.set_layout(layout=[[1,1,1,1,1,1,1,1,1,1],
-                                [1,0,0,0,0,0,0,0,3,1],
-                                [1,0,0,0,0,0,0,0,0,1],
-                                [1,0,0,4,0,0,0,0,0,1],
+                                [1,8,0,0,0,0,0,0,3,1],
+                                [1,0,0,0,0,5,0,0,0,1],
+                                [1,7,0,4,0,0,0,0,0,1],
                                 [1,0,0,0,0,0,0,0,0,1],
                                 [1,2,0,0,0,0,0,0,0,1],
                                 [1,1,1,1,1,1,1,1,1,1]])
         
+
 
 
 
--- src/model.py
+++ src/model.py
@@ -7,83 +7,164 @@ class Model:
         self.layout = map.get_layout()
         self.current_pos = map.get_start_pos()
         self.goal_pos = map.get_goal_pos()
+        self.stand_on = 0
 
 
     def check_win(self):
-        if self.current_pos == self.goal_pos:
+        self.balls_gone()
+        if self.current_pos == self.goal_pos and self.balls_gone():
             print("Gewonnen")
             return True
         else:
             return False 
+        
+    #Alle Bälle sind Weg
+    def balls_gone(self):
+        for x in self.layout:
+            for y in x:
+                if y == 7 or y == 9:
+                    return False
+        return True
+                
+        
+        
+    #Zahlensheet
+    #0=Frei, 1=Mauer, 2=Pushy, 3=Haus, 4=Kiste, 5=Wasser, 6=KisteWasser, 7=Ball_rot, 8=Lager_Rot, 9=Ball_grün
+    
 
     def move(self,pDirection):
         direction = pDirection.upper()
+        #UP________________________________________________________________________________________
         if direction == "UP" or direction == "U":
             if self.layout[self.current_pos[0]-1][self.current_pos[1]] == 1:
                 print("Mauer!!!")
             elif self.layout[self.current_pos[0]-1][self.current_pos[1]] == 4:
                 self.push(self.current_pos,"UP",4)
-            else:
-                self.layout[self.current_pos[0]][self.current_pos[1]] = 0
+            elif self.layout[self.current_pos[0]-1][self.current_pos[1]] == 7:
+                self.push(self.current_pos,"UP",7)
+
+            if self.layout[self.current_pos[0]-1][self.current_pos[1]] == 0:
+                self.layout[self.current_pos[0]][self.current_pos[1]] = self.stand_on
                 self.current_pos[0] -= 1
                 self.layout[self.current_pos[0]][self.current_pos[1]] = 2
+                self.stand_on = 0
+            elif self.layout[self.current_pos[0]-1][self.current_pos[1]] == 6:
+                self.layout[self.current_pos[0]][self.current_pos[1]] = self.stand_on
+                self.current_pos[0] -= 1
+                self.layout[self.current_pos[0]][self.current_pos[1]] = 2
+                self.stand_on = 6
+            elif self.layout[self.current_pos[0]-1][self.current_pos[1]] == 3:
+                self.layout[self.current_pos[0]][self.current_pos[1]] = self.stand_on
+                self.current_pos[0] -= 1
+                self.layout[self.current_pos[0]][self.current_pos[1]] = 2
+                self.stand_on = 3
+        #DOWN______________________________________________________________________________________
         elif direction == "DOWN" or direction == "D":
             if self.layout[self.current_pos[0]+1][self.current_pos[1]] == 1:
                 print("Mauer!!!")
             elif self.layout[self.current_pos[0]+1][self.current_pos[1]] == 4:
                 self.push(self.current_pos,"DOWN",4)
-            else:
-                self.layout[self.current_pos[0]][self.current_pos[1]] = 0
+            elif self.layout[self.current_pos[0]+1][self.current_pos[1]] == 7:
+                self.push(self.current_pos,"DOWN",7)
+
+
+            if self.layout[self.current_pos[0]+1][self.current_pos[1]] == 0:
+                self.layout[self.current_pos[0]][self.current_pos[1]] = self.stand_on
                 self.current_pos[0] += 1
                 self.layout[self.current_pos[0]][self.current_pos[1]] = 2
+                self.stand_on = 0
+            elif self.layout[self.current_pos[0]+1][self.current_pos[1]] == 6:
+                self.layout[self.current_pos[0]][self.current_pos[1]] = self.stand_on
+                self.current_pos[0] += 1
+                self.layout[self.current_pos[0]][self.current_pos[1]] = 2
+                self.stand_on = 6
+            elif self.layout[self.current_pos[0]+1][self.current_pos[1]] == 3:
+                self.layout[self.current_pos[0]][self.current_pos[1]] = self.stand_on
+                self.current_pos[0] += 1
+                self.layout[self.current_pos[0]][self.current_pos[1]] = 2
+                self.stand_on = 3
+        #LEFT______________________________________________________________________________________
         elif direction == "LEFT" or direction == "L":
             if self.layout[self.current_pos[0]][self.current_pos[1]-1] == 1:
                 print("Mauer!!!")
             elif self.layout[self.current_pos[0]][self.current_pos[1]-1] == 4:
                 self.push(self.current_pos,"LEFT",4)
-            else:
-                self.layout[self.current_pos[0]][self.current_pos[1]] = 0
+            elif self.layout[self.current_pos[0]][self.current_pos[1]-1] == 7:
+                self.push(self.current_pos,"LEFT",7)
+
+            if self.layout[self.current_pos[0]][self.current_pos[1]-1] == 0:
+                self.layout[self.current_pos[0]][self.current_pos[1]] = self.stand_on
                 self.current_pos[1] -= 1
                 self.layout[self.current_pos[0]][self.current_pos[1]] = 2
+                self.stand_on = 0
+            elif self.layout[self.current_pos[0]][self.current_pos[1]-1] == 6:
+                self.layout[self.current_pos[0]][self.current_pos[1]] = self.stand_on
+                self.current_pos[1] -= 1
+                self.layout[self.current_pos[0]][self.current_pos[1]] = 2
+                self.stand_on = 6
+            elif self.layout[self.current_pos[0]][self.current_pos[1]-1] == 3:
+                self.layout[self.current_pos[0]][self.current_pos[1]] = self.stand_on
+                self.current_pos[1] -= 1
+                self.layout[self.current_pos[0]][self.current_pos[1]] = 2
+                self.stand_on = 3
+        #RIGHT_____________________________________________________________________________________
         elif direction == "RIGHT" or direction == "R":
             if self.layout[self.current_pos[0]][self.current_pos[1]+1] == 1:
                 print("Mauer!!!")
             elif self.layout[self.current_pos[0]][self.current_pos[1]+1] == 4:
                 self.push(self.current_pos,"RIGHT",4)
-            else:
-                self.layout[self.current_pos[0]][self.current_pos[1]] = 0
+            elif self.layout[self.current_pos[0]][self.current_pos[1]+1] == 7:
+                self.push(self.current_pos,"RIGHT",7)
+
+            if self.layout[self.current_pos[0]][self.current_pos[1]+1] == 0:
+                self.layout[self.current_pos[0]][self.current_pos[1]] = self.stand_on
                 self.current_pos[1] += 1
                 self.layout[self.current_pos[0]][self.current_pos[1]] = 2
-
-    def push(self,pos,dir,obj):
-        if dir == "UP":
-            if self.layout[self.current_pos[0]-2][self.current_pos[1]] == 0: #Feld zum Pushen ist frei
-                self.layout[self.current_pos[0]-2][self.current_pos[1]] = obj
-                self.layout[self.current_pos[0]][self.current_pos[1]] = 0
-                self.current_pos[0] -= 1
-                self.layout[self.current_pos[0]][self.current_pos[1]] = 2
-        elif dir == "DOWN":
-            if self.layout[self.current_pos[0]+2][self.current_pos[1]] == 0: #Feld zum Pushen ist frei
-                self.layout[self.current_pos[0]+2][self.current_pos[1]] = obj
-                self.layout[self.current_pos[0]][self.current_pos[1]] = 0
-                self.current_pos[0] += 1
-                self.layout[self.current_pos[0]][self.current_pos[1]] = 2
-        elif dir == "LEFT":
-            if self.layout[self.current_pos[0]][self.current_pos[1]-2] == 0: #Feld zum Pushen ist frei
-                self.layout[self.current_pos[0]][self.current_pos[1]-2] = obj
-                self.layout[self.current_pos[0]][self.current_pos[1]] = 0
-                self.current_pos[1] -= 1
-                self.layout[self.current_pos[0]][self.current_pos[1]] = 2
-        elif dir == "RIGHT":
-            if self.layout[self.current_pos[0]][self.current_pos[1]+2] == 0: #Feld zum Pushen ist frei
-                self.layout[self.current_pos[0]][self.current_pos[1]+2] = obj
-                self.layout[self.current_pos[0]][self.current_pos[1]] = 0
+                self.stand_on = 0
+            elif self.layout[self.current_pos[0]][self.current_pos[1]+1] == 6:
+                self.layout[self.current_pos[0]][self.current_pos[1]] = self.stand_on
                 self.current_pos[1] += 1
                 self.layout[self.current_pos[0]][self.current_pos[1]] = 2
+                self.stand_on = 6
+            elif self.layout[self.current_pos[0]][self.current_pos[1]+1] == 3:
+                self.layout[self.current_pos[0]][self.current_pos[1]] = self.stand_on
+                self.current_pos[1] += 1
+                self.layout[self.current_pos[0]][self.current_pos[1]] = 3
+                self.stand_on = 6
+
+
+    def push(self, pos, dir, obj):
+        richtung = {
+        "UP":    (-1, 0),
+        "DOWN":  (1,  0),
+        "LEFT":  (0, -1),
+        "RIGHT": (0,  1)
+        }
+
+        if dir not in richtung:
+            return  # Ungültige Richtung, kein Push
+
+        dx, dy = richtung[dir]
+
+        # Start- & Zielkoordinaten berechnen
+        x, y = self.current_pos
+        ziel1 = (x + dx, y + dy)       # Feld direkt neben Spieler
+        ziel2 = (x + 2*dx, y + 2*dy)   # Feld nach dem geschobenen Objekt
+
+        zielwert = self.layout[ziel2[0]][ziel2[1]]
+
+        if zielwert == 0:
+            self.layout[ziel2[0]][ziel2[1]] = obj
+            self.layout[ziel1[0]][ziel1[1]] = 0
+        elif zielwert == 5 and obj == 4:  # Kiste in Wasser → Floß
+            self.layout[ziel2[0]][ziel2[1]] = 6
+            self.layout[ziel1[0]][ziel1[1]] = 0
+        elif zielwert == 8 and obj == 7:  # Ball ins Lager
+            self.layout[ziel2[0]][ziel2[1]] = 8
+            self.layout[ziel1[0]][ziel1[1]] = 0
 
 
 
-    
 
 
     def print_field(self):
@@ -91,7 +172,7 @@ class Model:
             print(x)
             
 
-m = map.Map()
+m = map.Map("Map1.json")
 g = Model(m)
 
 while not g.check_win():
@@ -99,5 +180,3 @@ while not g.check_win():
     g.move(dir)
     g.print_field()
     
-
-
